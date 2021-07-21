8656d0aa9b35022b8aa310904140d0da9be8e098
ovisannn <ovi.sanjaya11@gmail.com>
2021-07-21 07:23:08 +0700

    progress
    -adding shadow
    -updating buto script

--- prefabs/lifeObject/butoIjo.gd
+++ prefabs/lifeObject/butoIjo.gd
@@ -2,11 +2,16 @@ extends KinematicBody2D
 
 export var speed = 10
 export var health = 1000
-onready var obj = get_parent().get_node("bayu")
-var playerOnArea = false
 
+onready var spriteG = $Sprite
+onready var ani = $AnimationPlayer
+onready var obj = get_parent().get_node("bayu")
+
+var playerOnArea = false
 var doing = 'idle'
 var phase = 1
+var enemyArah = 'kiri'
+
 
 func doingFunction():
 	if playerOnArea == true:
@@ -14,35 +19,64 @@ func doingFunction():
 	if playerOnArea == false:
 		doing = 'chasing'
 		
-func phaseFunction():
-	if health < 700 and health > 300:
-		phase = 2
-	elif health <= 300:
-		phase = 3
-
-func attackPhase():
-	if phase == 1:
-		pass
-	if phase == 2:
-		pass
-	if phase == 3:
-		pass
-
-func animationControll():
-	pass
 		
+func attackPhase():
+	if health > 700:
+		#stomp
+		pass
+	if health < 700 and health > 300:
+		#stomp
+		#summon
+		pass
+	if health <= 300:
+		#stomp
+		#summon
+		#pattern ranged attack
+		pass
+
+
+func animationControll(arah):
+	if arah == 'kanan':
+		spriteG.flip_h = false
+	if arah == 'kiri':
+		spriteG.flip_h = true
+	
+	if doing == 'chasing':
+		ani.play("walk")
+	if doing == 'attack':
+		pass
+		
+
+
+func get_dir(direction):
+	var arah = 'kiri'
+	if direction[0] > 0:
+		arah = 'kanan'
+	if direction[0] < 0:
+		arah = 'kiri'
+	return arah
+	
+	
+#=======================================================================	
 func _ready():
 	pass
 	
 func _process(delta):
-	phaseFunction()
-	animationControll()
-	print(doing)
+	
+	animationControll(enemyArah)
+	
 	if doing == 'chasing':
 		var dir = (obj.global_position - global_position).normalized()
 		move_and_collide(dir * speed * delta)
+		enemyArah = get_dir(dir)
+		
 	if doing == 'attack':
 		attackPhase()
+		
+	if doing == 'dizzy':
+		pass
+#============================================================
+
 
 func _on_playerDetector_body_entered(body):
 	if body.name == 'bayu':
